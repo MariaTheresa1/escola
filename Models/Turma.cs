d5c3a4d7b4d82d9cb60fea93baeab923b2e0d9cb
Maria Theresa <mariatheresa98@hotmail.com>
2022-07-02 11:13:07 -0300

    classe Turma criada

---
+++ Models/Turma.cs
@@ -0,0 +1,38 @@
+﻿namespace escola.Models
+{
+    public class Turma
+    {
+        public int id;
+        public string nome;
+        public bool ativo;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public bool Ativo { get { return ativo; } set { ativo = value; } }
+
+        public Turma()
+        {
+
+        }
+        public void consultarTodasAsTurmas()
+        {
+
+        }
+        public void consultarTurmaPeloId()
+        {
+
+        }
+        public void incluirTurmas()
+        {
+
+        }
+        public void exluirTurmas()
+        {
+
+        }
+        public void atualizarTurmas()
+        {
+
+        }
+    }
+}
